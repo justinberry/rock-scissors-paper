741f0650c028046b9f082d8649b1dc3bdc08ac94
Justin Berry <justin.berry@protonmail.com>
2019-06-23 20:07:00 +1000

    Mess around with the model a bit more

--- src/dataset/factory.py
+++ src/dataset/factory.py
@@ -12,6 +12,9 @@ import random
 
 data_root = pathlib.Path('./data')
 
+def get_label_factory(label_names):
+  return lambda index : label_names[index]
+
 def get_image_paths():
   image_path_list = list(data_root.glob('*/*.png'))
   image_paths = [str(path) for path in image_path_list]
@@ -21,8 +24,8 @@ def get_image_paths():
 def create_labelled_images(image_paths):
   # Infer labels from directory names
   label_names = sorted(
-    item.name
-    for item in data_root.glob('*/') if item.is_dir()
+    item.name for item in data_root.glob('*/')
+      if item.is_dir()
   )
   # Create indexed labels for rock, scissors and paper.
   # Each image will receive one of these mapped indices as its
@@ -36,10 +39,10 @@ def create_labelled_images(image_paths):
     label_with_index[pathlib.Path(path).parent.name]
     for path in image_paths
   ]
-  return labelled_images, label_with_index, label_names
+  return labelled_images, get_label_factory(label_names)
 
 def create():
   image_paths = get_image_paths()
-  labelled_images, label_with_index, label_names = create_labelled_images(image_paths)
-  return (image_paths, labelled_images, label_with_index, label_names)
+  labelled_images, label_names = create_labelled_images(image_paths)
+  return (image_paths, labelled_images, label_names)
 #%%
--- src/main.py
+++ src/main.py
@@ -15,16 +15,12 @@ sys.path.append('./src')
 
 from dataset import factory
 
-image_paths, labelled_images, label_with_index, label_names = factory.create()
+def get_label_factory(label_names):
+  return lambda index : label_names[index]
+
+image_paths, labelled_images, get_label = factory.create()
 
 img_tensor = preprocess_path(image_paths[0])
-
-print('--------------')
-print(img_tensor.shape)
-print(img_tensor.dtype)
-
-print(label_with_index)
-#plot(img_tensor, labelled_images[0])
 
 path_dataset = tf.data.Dataset.from_tensor_slices(image_paths)
 
@@ -33,23 +29,11 @@ image_dataset = path_dataset.map(
   num_parallel_calls=AUTOTUNE
 )
 
-#plot_all(
-#  image_dataset=image_dataset,
-#  image_paths=image_paths,
-#  labelled_images=labelled_images
-#)
-
 label_dataset = tf.data.Dataset.from_tensor_slices(
   tf.cast(labelled_images, tf.int64)
-) 
-
-#for l in label_dataset.take(10):
-#  print(label_names[l.numpy()])
+)
 
 image_label_dataset = tf.data.Dataset.zip((image_dataset, label_dataset))
-
-print('image_label_dataset')
-print(image_label_dataset)
 
 # Basic methods for training
 #
@@ -68,9 +52,6 @@ BATCH_SIZE = 32
 #   A .shuffle after a .batch would shuffle the order of the batches, but not shuffle the items across batches.
 
 IMAGE_COUNT = len(image_paths)
-
-# batch = image_label_dataset.shuffle(buffer_size=IMAGE_COUNT)
-# repeated = batch.repeat()
 
 shuffled_and_repeated = image_label_dataset.cache().apply(
   tf.data.experimental.shuffle_and_repeat(buffer_size=IMAGE_COUNT)
@@ -92,51 +73,31 @@ keras_dataset = the_dataset.map(normalise_range)
 image_batch, label_batch = next(iter(keras_dataset))
 
 feature_map_batch = mobile_net(image_batch)
-print(feature_map_batch.shape)
+
+NUM_LABELS = 3
 
 model = tf.keras.Sequential([
   mobile_net,
   tf.keras.layers.GlobalAveragePooling2D(),
-  tf.keras.layers.Dense(len(label_names))])
+  tf.keras.layers.Dense(NUM_LABELS, activation='softmax')
+])
 
-logit_batch = model(image_batch).numpy()
+#model = tf.keras.applications.ResNet50(weights='imagenet')
 
-print("min logit:", logit_batch.min())
-print("max logit:", logit_batch.max())
-print()
+model.compile(optimizer=tf.keras.optimizers.SGD(learning_rate=0.0000001),
+              loss='categorical_crossentropy',
+              metrics=['accuracy'])
 
-print("Shape:", logit_batch.shape)
-
-model.compile(optimizer=tf.keras.optimizers.Adam(),
-              loss='sparse_categorical_crossentropy',
-              metrics=["accuracy"])
-
-print(len(model.trainable_variables))
+# The model is now created and ready to be fit against training set
 model.summary()
 
-steps_per_epoch=tf.math.ceil(len(image_paths) / BATCH_SIZE).numpy()
-print(steps_per_epoch)
+steps_per_epoch=tf.math.ceil(IMAGE_COUNT / BATCH_SIZE).numpy()
 
-#model.fit(the_dataset, epochs=50, steps_per_epoch=steps_per_epoch)
-model.fit(the_dataset, epochs=1, steps_per_epoch=3)
+model.fit(the_dataset, epochs=5, steps_per_epoch=steps_per_epoch * 2)
+#model.fit(the_dataset, epochs=1, steps_per_epoch=3)
 
-
-class_names = sorted(label_with_index.items(), key=lambda pair:pair[1])
-class_names = numpy.array([key.title() for key, value in class_names])
-
-predicted_batch = model.predict(the_dataset)
-print('predicted_batch')
-print(predicted_batch)
-predicted_id = tf.math.argmax(predicted_batch, axis=-1)
-print('predicted_id')
-print(predicted_id)
-predicted_label_batch = class_names[predicted_id]
-print('predicted_label_batch')
-print(predicted_label_batch)
-
-label_id = tf.math.argmax(label_batch, axis=-1)
-print('label_id')
-print(label_id)
+prediction = model.predict(the_dataset)
+prediction_indicies = tf.math.argmax(prediction, axis=-1)
 
 import matplotlib.pyplot as matplot
 
@@ -145,14 +106,16 @@ matplotlib.use('WebAgg')
 
 matplot.figure(figsize=(10,9))
 matplot.subplots_adjust(hspace=0.5)
-for n in range(30):
-  matplot.subplot(6,5,n+1)
+plot_x_width = 6
+plot_y_width = 5
+for n in range(plot_x_width * plot_y_width):
+  matplot.subplot(plot_x_width, plot_y_width, n+1)
   matplot.imshow(image_batch[n])
-#  color = "green" if predicted_id[n] == label_id[n] else "red"
-  color = "green"
-  matplot.title(predicted_label_batch[n].title(), color=color)
+  matplot.title(get_label(prediction_indicies[n]), color='black')
   matplot.axis('off')
-_ = matplot.suptitle("Model predictions (green: correct, red: incorrect)")
+_ = matplot.suptitle('Model predictions')
 matplot.show()
 
 # 
+
+#%%
